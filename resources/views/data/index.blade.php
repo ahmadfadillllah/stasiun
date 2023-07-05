fbb6e329957b88353f2db95f56b05fdda6a5a087
Ahmad Fadillah <13020180149@umi.ac.id>
2023-07-05 16:10:33 +0800

    update

--- resources/views/data/index.blade.php
+++ resources/views/data/index.blade.php
@@ -28,7 +28,7 @@
                 @include('notif.alert')
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="mt-0 header-title">Daftar Stasiun</h4>
+                        <h4 class="mt-0 header-title">Daftar Ketersediaan Data</h4>
                         <table id="datatable" class="table table-bordered dt-responsive nowrap"
                             style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                             <thead>
